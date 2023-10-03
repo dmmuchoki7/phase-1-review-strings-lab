9f711cde3415d71ec1e7990168ae0297adf548ed
dmmuchoki7 <dmmuchoki7@gmail.com>
2023-10-03 11:55:12 +0300

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 // Write your code in this file!
+const currentUser = "Grace Hopper";
+
+// welcome message
+const welcomeMessage= "Welcome to Flatbook, "+ currentUser + "!";
+
+//excitedWelcomeMessage
+const excitedWelcomeMessage= welcomeMessage.toUpperCase();
+
+//shortGreeting
+const shortGreeting="Welcome, " +currentUser.slice(0,11)+ "!";
+
+
+
